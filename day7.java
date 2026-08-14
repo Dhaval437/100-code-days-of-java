014d589fbab9d2a1ddc75017929e84cf95f65020
Dhaval437 <dhavalgandhi98221@gmail.com>
2026-08-15 00:10:07 +0530

    day7 code

---
+++ day7.java
@@ -0,0 +1,14 @@
+public class day7 {
+    public static void printpattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=4;
+        printpattern(n);
+    }
+}
